62a7fa7cde0cd78b7ed053cac1681b4da0542e58
michaelhill4 <106322946+michaelhill4@users.noreply.github.com>
2022-09-29 22:34:21 -0400

    added service worker and html plugin

---
+++ Develop/client/src/js/register-sw.js
@@ -0,0 +1,10 @@
+export const registerSW = () => {
+
+    if (
+        'serviceWorker' in navigator
+    ) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js');
+        });
+    }
+}
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,6 +4,35 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
+
+new HtmlWebpackPlugin({
+  template: './index.html',
+  title: 'Contact Cards'
+}),
+
+
+
+new WebpackPwaManifest({
+  fingerprints: false,
+  inject: true,
+  name: 'Contact Cards',
+  short_name: 'Contact',
+  description: 'Never forget your contacts!',
+  background_color: '#225ca3',
+  theme_color: '#225ca3',
+  start_url: './',
+  publicPath: './',
+  icons: [
+    {
+      src: path.resolve('src/images/logo.png'),
+      sizes: [96, 128, 192, 256, 384, 512],
+      destination: path.join('assets', 'icons'),
+    },
+  ],
+}),
+
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
