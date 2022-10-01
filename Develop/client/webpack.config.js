b077b59e641020f2e2402dd0518f32b4ba235884
michaelhill4 <106322946+michaelhill4@users.noreply.github.com>
2022-09-30 22:22:20 -0400

    added loaders and ran webpack working fine

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -6,19 +6,36 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 
 
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      
+
 new HtmlWebpackPlugin({
   template: './index.html',
   title: 'Contact Cards'
 }),
 
-
+new InjectManifest({
+  swSrc: './src-sw.js',
+  swDest: 'src-sw.js',
+}),
 
 new WebpackPwaManifest({
   fingerprints: false,
   inject: true,
-  name: 'Contact Cards',
-  short_name: 'Contact',
-  description: 'Never forget your contacts!',
+  name: 'Just Another Text Editor',
+  short_name: 'J.A.T.E.',
+  description: 'Juavascript Text Editor',
   background_color: '#225ca3',
   theme_color: '#225ca3',
   start_url: './',
@@ -35,24 +52,25 @@ new WebpackPwaManifest({
 
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }
       ],
     },
   };
